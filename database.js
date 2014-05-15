496662b09887820e70fe4d454ded524f70bd6d3c
no-glue <nikolapav1985@gmail.com>
2014-05-15 13:19:14 +0200

    tips as a module

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var dbUrl = 'tips';
 // live 'mongodb://heroku_app24172037:snm2u7bejp2qmu66nu61lav9lo@ds053718.mongolab.com:53718/heroku_app24172037'
 
-var collections = ['tips'];
+var collections = ['tips', 'users'];
 
 var db = require('mongojs').connect(dbUrl, collections);
 
--- routes/index.js
+++ routes/index.js
@@ -1,100 +1,96 @@
+var tips = (function() {
+  var Tips = function() {
+    var root = this;
 
-/*
- * GET home page.
- */
+    root.set = function(options) {
+      // set options
+      for(var key in options)root[key] = options[key];
 
-var Tips = function() {
-  var root = this;
+      return root;
+    };
 
-  root.set = function(options) {
-    // set options
-    for(var key in options)root[key] = options[key];
+    root.get = function(key) {
+      // get key
+      return root[key];
+    };
 
-    return root;
+    root.index = function(req, res) {
+      // show index view
+      res.render('index', {title: 'Tip'});
+    };
+
+    root.all = function(req, res) {
+      // gets all tips
+      root.get('database')[root.get('collection')].find().limit(16384).sort({_id: -1}, function(err, tips) {
+        if(err) return;
+
+        res.json(tips);
+      });
+    };
+
+    root.tip = function(req, res) {
+      // gets a tip according to id
+      var db = root.get('database');
+
+      var id = db.ObjectId(req.params.id);
+
+      db[root.get('collection')].findOne({_id: id}, function(err, tip) {
+        if(err) return;
+
+        res.json(tip);
+      });
+    };
+
+    root.tag = function(req, res) {
+      // gets tips according to tag
+      var tag = req.params.name;
+
+      root.get('database')[root.get('collection')].find({tag: tag}, function(err, tips) {
+        if(err) return;
+
+        res.json(tips);
+      });
+    };
+
+    root.name = function(req, res) {
+      // gets tips according to name
+      var name = req.params.name;
+
+      root.get('database')[root.get('collection')].find({name: name}, function(err, tips) {
+        if(err) return;
+
+        res.json(tips);
+      });
+    };
+
+    root.create = function(req, res) {
+      // adds a tip
+      root.get('database')[root.get('collection')].save(req.body, function(err, saved) {
+        res.json(req.body);
+      });
+    };
+
+    root.update = function(req, res) {
+      // updates tip
+
+      var db = root.get('database');
+
+      var id = db.ObjectId(req.body._id);
+
+      delete req.body._id;
+
+      root.getDatabase()[root.get('collection')].update({_id: id}, {$set: req.body}, function(err, lastErrorObject) {
+        req.body._id = id;
+
+        res.json(req.body);        
+      });
+    };
   };
 
-  root.get = function(key) {
-    // get key
-    return root[key];
-  };
+  return new Tips();
+})();
 
-  root.index = function(req, res) {
-    // show index view
-    res.render('index', {title: 'Tip'});
-  };
-
-  root.all = function(req, res) {
-    // gets all tips
-    root.get('database')[root.get('collection')].find().limit(16384).sort({_id: -1}, function(err, tips) {
-      if(err) return;
-
-      res.json(tips);
-    });
-  };
-
-  root.tip = function(req, res) {
-    // gets a tip according to id
-    var db = root.get('database');
-
-    var id = db.ObjectId(req.params.id);
-
-    db[root.get('collection')].findOne({_id: id}, function(err, tip) {
-      if(err) return;
-
-      res.json(tip);
-    });
-  };
-
-  root.tag = function(req, res) {
-    // gets tips according to tag
-    var tag = req.params.name;
-
-    root.get('database')[root.get('collection')].find({tag: tag}, function(err, tips) {
-      if(err) return;
-
-      res.json(tips);
-    });
-  };
-
-  root.name = function(req, res) {
-    // gets tips according to name
-    var name = req.params.name;
-
-    root.get('database')[root.get('collection')].find({name: name}, function(err, tips) {
-      if(err) return;
-
-      res.json(tips);
-    });
-  };
-
-  root.create = function(req, res) {
-    // adds a tip
-    root.get('database')[root.get('collection')].save(req.body, function(err, saved) {
-      res.json(req.body);
-    });
-  }
-
-  root.update = function(req, res) {
-    // updates tip
-
-    var db = root.get('database');
-
-    var id = db.ObjectId(req.body._id);
-
-    delete req.body._id;
-
-    root.getDatabase()[root.get('collection')].update({_id: id}, {$set: req.body}, function(err, lastErrorObject) {
-      req.body._id = id;
-
-      res.json(req.body);        
-    });
-  }
-};
-
-var tips = new Tips()
-  .set({
-    database: require('../database'),
-    collection: 'tips'
-  });
-
-exports.tips = tips;
+exports.tips = tips.set({
+  database: require('../database'),
+  collection: 'tips'
+});
