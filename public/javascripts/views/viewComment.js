64b47fa75dc54d8781cbf8e0ee66716c26258f4d
no-glue <nikolapav1985@gmail.com>
2014-05-17 00:40:01 +0200

    comment template

--- public/javascripts/views/viewComment.js
+++ public/javascripts/views/viewComment.js
@@ -1,7 +1,11 @@
 var CommentView = View.extend({
   tagName: 'li',
   render: function() {
-    this.$el.html(this.model.attributes.content);
+    var html = $('#commentTemplate').html();
+
+    var compiled = Handlebars.compile(html);
+
+    this.$el.html(compiled(this.model.attributes));
 
     return this;
   }
