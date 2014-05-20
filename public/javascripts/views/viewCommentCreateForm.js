3e5bce42f24c65b6923ccf5d4396e9ab404381df
no-glue <nikolapav1985@gmail.com>
2014-05-20 20:27:51 +0200

    comment write area

--- public/javascripts/views/viewCommentCreateForm.js
+++ public/javascripts/views/viewCommentCreateForm.js
@@ -1,6 +1,8 @@
 var CommentCreateFormView = View.extend({
+  tagName: 'form',
+  className: 'centre',
   render: function() {
-    this.$el.html('comment create');
+    this.$el.html($('#commentCreateFormTemplate').html());
 
     return this;
   }
