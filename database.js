213a2881a5cf6b95d162b46c883538278c78e256
no-glue <nikolapav1985@gmail.com>
2014-06-14 20:08:35 +0200

    db options

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 var dbUrl = 'tips';
+// 'tips'
 // live 'mongodb://heroku_app24172037:snm2u7bejp2qmu66nu61lav9lo@ds053718.mongolab.com:53718/heroku_app24172037'
 
 var collections = ['tips', 'comments', 'users'];
